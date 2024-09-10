112db8f2b19165ee06f0e1f6c686b5beafd53d73
harinibaskar <hbaskar@calpoly.edu>
2024-09-10 12:09:45 -0700

    "message"

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typed-text", {
+  strings: ["About MSe:"],  // The text to type out
+  typeSpeed: 50,               // Speed at which the text is typed
+  backSpeed: 0,                // No speed for backspacing
+
+  startDelay: 500,             // Delay before typing starts
+  loop: true,                 // Do not loop the animation
+  showCursor: true             // Show the cursor (set to false if you don't want it)
+});
